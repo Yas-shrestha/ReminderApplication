7bc86233439f4d9a071a80e09b00d2a875c60d4c
Yas Shrestha <yeskumarshrestha1@gmail.com>
2026-05-06 22:32:46 +0545

    [Add]:Filament Resource added

--- app/Models/Reminder.php
+++ app/Models/Reminder.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Reminder extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'user_id',
         'title',
@@ -14,9 +17,21 @@ class Reminder extends Model
         'isSent',
         'sent_at',
     ];
+    // cast the remind_at and sent_at fields to datetime and isSent to boolean
+    protected $casts = [
+        'remind_at' => 'datetime',
+        'sent_at'   => 'datetime',
+        'is_sent'   => 'boolean',
+    ];
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    protected static function booted(): void
+    {
+        static::creating(function ($reminder) {
+            $reminder->user_id ??= Auth::id();
+        });
     }
 }
